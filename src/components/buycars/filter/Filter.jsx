22902c1346a1c1d54f3f74aa5c5977db976b5c81
Myloveiszero <2446730006@qq.com>
2020-07-19 23:28:10 +0800

    list组件

--- src/components/buycars/filter/Filter.jsx
+++ src/components/buycars/filter/Filter.jsx
@@ -5,18 +5,26 @@ function Filter(){
 
     return(
         <div className="filter">
-                <nav className="filter-nav">
-                    <a href="" className="nav-item"><span>价格最低</span></a>
-                    <a href="" className="nav-item">品牌</a>
-                    <a href="" className="nav-item">价格</a>
-                    <a href="" className="nav-item">筛选</a>
-                </nav>
-                <nav className="filter-nav-third">
+                <div className="filter-nav">
+                    <a href="" className="nav-item order">
+                        <span>价格最低</span>
+                    </a>
+                    <a href="" className="nav-item brand">
+                        <span>品牌</span
+                    ></a>
+                    <a href="" className="nav-item price">
+                        <span>价格</span>
+                    </a>
+                    <a href="" className="nav-item option">
+                        <span>筛选</span>
+                    </a>
+                </div>
+                <div className="filter-nav-third">
                     <a href="" className="third-nav-item-icon">严选好车</a>
                     <a href="" className="third-nav-item-icon">全国低价</a>
                     <a href="" className="third-nav-item-icon">准新车</a>
                     <a href="" className="third-nav-item-icon">限时秒强</a>
-                </nav>
+                </div>
         </div>
     )
 }
--- src/components/buycars/list/List.jsx
+++ src/components/buycars/list/List.jsx
@@ -4,8 +4,69 @@ import './List.css';
 function List() {
 
     return (
-        <div>
-            List
+        <div className="list-wrapper">
+            <div className="list-content">
+                <div className="img-wrapper"></div>
+                <div className="info-wrapper">
+                    <div className="car-name">
+                        <h3>三菱 欧蓝德 2016款 2.4L 四驱精英版 5座</h3>
+                    </div>
+                    <div className="car-time">
+                        <div className="car-info"></div>
+                        <div className="preferential"></div>
+                    </div>
+                    <div className="car-price">
+                        <span className="pic-num"></span>
+                        <div className="discount"></div>
+                        <div className="trolley-icon"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="list-content">
+                <div className="img-wrapper"></div>
+                <div className="info-wrapper">
+                    <div className="car-name"></div>
+                    <div className="car-time">
+                        <div className="car-info"></div>
+                        <div className="preferential"></div>
+                    </div>
+                    <div className="car-price">
+                        <span className="pic-num"></span>
+                        <div className="discount"></div>
+                        <div className="trolley-icon"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="list-content">
+                <div className="img-wrapper"></div>
+                <div className="info-wrapper">
+                    <div className="car-name"></div>
+                    <div className="car-time">
+                        <div className="car-info"></div>
+                        <div className="preferential"></div>
+                    </div>
+                    <div className="car-price">
+                        <span className="pic-num"></span>
+                        <div className="discount"></div>
+                        <div className="trolley-icon"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="list-content">
+                <div className="img-wrapper"></div>
+                <div className="info-wrapper">
+                    <div className="car-name"></div>
+                    <div className="car-time">
+                        <div className="car-info"></div>
+                        <div className="preferential"></div>
+                    </div>
+                    <div className="car-price">
+                        <span className="pic-num"></span>
+                        <div className="discount"></div>
+                        <div className="trolley-icon"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
